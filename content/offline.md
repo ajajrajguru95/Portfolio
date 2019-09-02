23e84be0700c10fcdce8da1584065de9b39153f1
Ajaj Rajguru <ajajrajguru95@gmail.com>
2019-09-02 16:00:36 +0530

    PWA service worker for offline caching

---
+++ content/offline.md
@@ -0,0 +1,6 @@
+---
+date: 2019-07-28T06:18:29.416Z
+draft: false
+title: Oops, you are offline.
+type: offline
+---
--- static/js/site-scripts.js
+++ static/js/site-scripts.js
@@ -35,3 +35,74 @@ const imgObserver = new IntersectionObserver((entries,imgObserver) => {
 images.forEach(image => {
 	imgObserver.observe(image);
 });
+
+
+// Service worker for PWA
+
+addEventListener('message', messageEvent => {
+  if (messageEvent.data === 'skipWaiting') return skipWaiting();
+});
+
+addEventListener('fetch', event => {
+  event.respondWith((async () => {
+    if (event.request.mode === "navigate" &&
+      event.request.method === "GET" &&
+      registration.waiting &&
+      (await clients.matchAll()).length < 2
+    ) {
+      registration.waiting.postMessage('skipWaiting');
+      return new Response("", {headers: {"Refresh": "0"}});
+    }
+    return await caches.match(event.request) ||
+      fetch(event.request);
+  })());
+});
+
+
+
+function listenForWaitingServiceWorker(reg, callback) {
+  function awaitStateChange() {
+    reg.installing.addEventListener('statechange', function() {
+      if (this.state === 'installed') callback(reg);
+    });
+  }
+  if (!reg) return;
+  if (reg.waiting) return callback(reg);
+  if (reg.installing) awaitStateChange();
+  reg.addEventListener('updatefound', awaitStateChange);
+}
+
+// reload once when the new Service Worker starts activating
+var refreshing;
+navigator.serviceWorker.addEventListener('controllerchange',
+  function() {
+    if (refreshing) return;
+    refreshing = true;
+    window.location.reload();
+  }
+);
+function promptUserToRefresh(reg) {
+  // this is just an example
+  // don't use window.confirm in real life; it's terrible
+  if (window.confirm("New version available! OK to refresh?")) {
+    reg.waiting.postMessage('skipWaiting');
+  }
+}
+listenForWaitingServiceWorker(reg, promptUserToRefresh);
+
+
+
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('/sw.js', { scope: '/' })
+        .then(function(registration) {
+            console.log('Service Worker Registered');
+            registration.update();
+        });
+
+    navigator.serviceWorker
+        .ready
+        .then(function(registration) {
+            console.log('Service Worker Ready');
+        });
+}
